561db66aed0e5ca7cadffaecb741783a1eebab24
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-02-19 14:19:13 +0200

    Rewritten according to new commits

--- src/MainTest/TestCases.java
+++ src/MainTest/TestCases.java
@@ -11,21 +11,19 @@ public class TestCases {
     GameControl gc;
     @Before
     public void preparation() {
-        bm = new BoardModel();
-        bm.setup(8, 8, new ClassicChessTileGenerator());
+        bm = new BoardModel(8, 8, new ClassicChessTileGenerator());
     }
     @Test
     public void testCase1() {
         String command = "e2 e4";
         BoardMove move = BoardMove.parseBoardMove(command);
-        Tile currentTile = bm.getTile(move.getStartXCoord(), move.getStartYCoord());
-        boolean result = currentTile.getPlacedFigure().validateMove(move, bm);
+        boolean result = bm.validateMove(move);
         assertTrue(result);
     }
 
     @Test
     public void testCase2() {
-        gc = new GameControl();
+        gc = new GameControl(new CLIWorker());
         gc.start();
     }
 }
