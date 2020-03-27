40446359a212c8ca7f24ca3e1896e631f8ab7a66
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-03-27 16:55:30 +0200

    Logging UI class made to work with any inputStream instead of System.in

--- src/main/java/chess/UI/LoggingUIWorkerImpl.java
+++ src/main/java/chess/UI/LoggingUIWorkerImpl.java
@@ -3,20 +3,25 @@ package chess.UI;
 import chess.BoardMove;
 import org.apache.logging.log4j.Logger;
 
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class LoggingUIWorkerImpl extends LoggingUIWorker {
 
-    protected Scanner systemInputScanner;
+    protected Scanner inputScanner;
 
     public LoggingUIWorkerImpl(Logger logOutput, Logger logError) {
         super(logOutput, logError);
-        systemInputScanner = new Scanner(System.in);
+        inputScanner = new Scanner(System.in);
+    }
+
+    public LoggingUIWorkerImpl(InputStream inputStream, Logger logOutput, Logger logError) {
+        super(logOutput, logError);
+        inputScanner = new Scanner(inputStream);
     }
 
     @Override
     public BoardMove getMoveCommand(String outputMessage) {
-        System.out.println("Enter new command. Format: \"x0 x0\"");
-        return BoardMove.parseBoardMove(systemInputScanner.nextLine());
+        return BoardMove.parseBoardMove(inputScanner.nextLine());
     }
 }
